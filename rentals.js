c9aa1d64307a12ced59b62e82c7b1806d8453bfa
David Feltell <dave@feltell.net>
2016-10-30 01:43:56 +0100

    Removed confusing use of `Q` library from rentals script
    * Plus fixed/added min rating and num weeks info to title.
    * Also removed `q` dependency in package.json.

--- rentals.js
+++ rentals.js
@@ -3,7 +3,6 @@
 var step = 0,
 	Casper = require("casper"),
 	_ = require('lodash'),
-	Q = require("q"),
 	fs = require("fs"),
 	options = {
 		verbose: true,
@@ -17,7 +16,8 @@ var step = 0,
 	casper = Casper.create(options),
 	numWeeks = parseInt(casper.cli.args[0] || "8", 10),
 	minRating = parseFloat(casper.cli.args[1] || "6.0", 10),
-	noop = function () {};
+	noop = function () {},
+	titles;
 
 
 casper.echo("Starting");
@@ -25,14 +25,12 @@ casper.start("about:blank");
 
 
 function charts() {
-	var deferred = Q.defer(),
-		page = casper;//Casper.create(options);
-
+	var page = casper;//Casper.create(options);
 
 	page.thenOpen("http://www.officialcharts.com/charts/film-on-video-chart/");
 
 	page.then(function () {
-		var titles = this.evaluate(function () {
+		titles = this.evaluate(function () {
 			return $("table.chart-positions tr").not("[class]").map(function () {
 				return {
 					weeks: parseInt($("td", this).last().prev().text().trim(), 10),
@@ -44,24 +42,23 @@ function charts() {
 		titles = _(titles).filter(function (title) {
 			return title.weeks <= numWeeks;
 		}).sortBy('weeks').value();
-
-		deferred.resolve(titles);
 	});
-
-	return deferred.promise;
 }
 
 
 function imdbLink (title) {
-	var deferred = Q.defer(),
-		page = casper;//Casper.create(options);
+	var page = casper;//Casper.create(options);
+
+	page.then(function () {
+		page.echo("Started searching IMDB link for '" + title.name + "'");
+	});
 
 	page.thenOpen(
 		"http://www.imdb.com/find?q=" + encodeURIComponent(title.name)
 	);
 
 	page.then(function () {
-		page.echo("Started searching IMDB link for '" + title.name + "'");
+		page.echo("Waiting for IMDB link for '" + title.name + "'");
 	});
 
 	page.waitForSelector("#main");
@@ -81,17 +78,12 @@ function imdbLink (title) {
 			title.url = title.url.split("?")[0];
 			this.echo("IMDB link for '" + title.name + "' is " + title.url);
 		}
-
-		deferred.resolve(title);
 	});
-
-	return deferred.promise;
 }
 
 
 function imdb(title) {
-	var deferred = Q.defer(),
-		page = casper;//Casper.create(options);
+	var page = casper;//Casper.create(options);
 
 	page.then(function () {
 		page.echo("Opening IMDB info for '" + title.name + "' from " + title.url);
@@ -126,11 +118,7 @@ function imdb(title) {
 		});
 
 		_.extend(title, imdbInfo);
-
-		deferred.resolve(title);
 	});
-
-	return deferred.promise;
 }
 
 
@@ -139,28 +127,40 @@ casper.echo(
 );
 
 
-charts().then(function (titles) {
-	return Q.all(titles.map(imdbLink));
-}).then(function (titles) {
+charts();
+
+casper.then(function () {
+	casper.echo("getting imdb links");
+	titles.forEach(imdbLink);
+});
+
+casper.then(function () {
+	casper.echo("filtering out titles with no imdb link");
 	titles = titles.filter(function (title) {
 		return !!title.url;
 	});
 	titles = _.uniqBy(titles, "url");
-	return Q.all(titles.map(imdb));
-}).then(function (titles) {
+
+	casper.echo("getting imdb info");
+	titles.forEach(imdb);
+});
+
+casper.then(function () {
 	var html;
+
+	casper.echo("filtering out titles less than min rating");
 
 	titles = titles.filter(function (title) {
 		return title.rating >= minRating;
 	});
 
-	this.echo("constructing html for remaining " + titles.length + " shows");
+	casper.echo("constructing html for remaining " + titles.length + " shows");
 
 	html = "<thead><th>Title</th><th>Rating</th><th>Weeks</th><th>Genre</th</thead><tbody>";
 
 	titles.forEach(function (title) {
 		html += "<tr><th>" + title.name + " (" + title.year + ")" + "</th><th>" +
-			(title.rating && title.rating.toFixed(2) || "???") +
+			(title.rating && title.rating.toFixed(1) || "???") +
 			"</th><th>" + title.weeks + "</th><th>" + title.genre +  "</th></tr>" +
 			"<tr><td colspan=4>" + title.description + "<br/>" +
 			"<a href='" + title.url + "'>" + title.url + "</a></td></tr>";
@@ -180,13 +180,13 @@ charts().then(function (titles) {
 
 	html = tmpl({
 		page: "movierentals", headerTitle: "Top Movie Rentals",
-		headerSubtitle: "overview of highly rated movie rentals from the last",
+		headerSubtitle: "overview of movie rentals rated greater than " + minRating.toFixed(1) +
+		" from the last " + numWeeks + " weeks",
 		tableContent: html, listSrcURL: "http://www.officialcharts.com",
 		listSrcName: "officialcharts.com"
 	});
 
 	this.echo("writing html");
-
 
 	fs.write("movierentals.html", html, { mode: "w", charset: "utf-8" });
 });
